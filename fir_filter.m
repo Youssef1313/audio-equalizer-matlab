d00a9c818f5a2d31bb884af9de37ebdc12ab273b
MOHAND99 <muhanned122000@yahoo.com>
2021-06-11 20:47:52 +0200

    edit loop

--- fir_filter.m
+++ fir_filter.m
@@ -1,6 +1,7 @@
 function [ filters ] = fir_filter( order, fs, bands)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-filters = fir1(order + 1, fs / 2, bands / 2); 
+for band = bands
+filters = [filters fir1(order + 1, fs / 2, bands / 2)]; 
 end
 
