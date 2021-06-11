a0e0ce36f532b7b0167338a278ef79e6b1a5f8eb
mohamed-zayton <mohamed-zayton>
2021-06-11 21:28:39 +0200

    iir

---
+++ helper_method.m
@@ -0,0 +1,15 @@
+band =[0 170;170 310;310 600;600 1000;1000 3000;3000 6000;6000 12000;12000 14000;14000 16000];
+function filters = iir_filters(order,fs)
+filters = []
+band_norm = (band * 2)/(fs);
+    for i = 1:2:18
+        if band_norm(i+1) >= 1
+            return
+        end
+        if band(i) == 0
+            filters(i) = butter(5,band_norm(2)); 
+        else
+            filter(i) =  butter(3,band_norm(i,;),'bandpass'); 
+        end
+    end
+end
