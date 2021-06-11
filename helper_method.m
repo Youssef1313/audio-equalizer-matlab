2563366e7c9293a23748cdc8723fe3e19b37613b
mohamed-zayton <mohamed-zayton>
2021-06-11 23:07:36 +0200

    iir

--- helper_method.m
+++ helper_method.m
@@ -1,17 +1,19 @@
 
-function filters = iir_filters(order,fs)
-    filters = []
-    band = get_bands();
-    band_norm = (band * 2)/(fs);
+function filters = iir_filters(order,fs,bands)
+    filters = [];
+    band_norm = (bands * 2) / (fs) ;
     for i = 1:9
         if band_norm(i+1) >= 1
             return
         end
         if band(i) == 0
-            filters(i,:) = butter(5,band_norm(2)); 
+            [b a] = butter(5,band_norm(2));
+            
         else
-            filter(i,:) =  butter(3,band_norm(i,:),'bandpass'); 
+            [b a] =  butter(3,band_norm(i,:),'bandpass'); 
         end
+        filter = Filters(b, a)
+        filters = [filters filter]
     end
 end
 
