a43958db7f1a6b93cd4d055141d9114844a198c7
mohamed-zayton <mohamed-zayton>
2021-06-11 23:46:17 +0200

    iir

---
+++ iir_filters.m
@@ -0,0 +1,17 @@
+function filters = iir_filters(order,fs,bands)
+    filters = [];
+    band_norm = (bands * 2) / (fs) ;
+    for i = 1:9
+        if band_norm(i+1) >= 1
+            return
+        end
+        if band(i) == 0
+            [b a] = butter(5,band_norm(2));
+            
+        else
+            [b a] =  butter(3,band_norm(i,:),'bandpass'); 
+        end
+        filter = Filters(b, a)
+        filters = [filters filter]
+    end
+end
