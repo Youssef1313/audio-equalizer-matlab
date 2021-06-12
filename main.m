44e32b80dcdf42ad82ea2a9842352eb1bc19ef7b
Muhannad Muhammad Bashar <53486163+MOHAND99@users.noreply.github.com>
2021-06-13 00:46:56 +0200

    filtering (#5)
    * filtering
    
    * Apply suggestions from code review
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- main.m
+++ main.m
@@ -30,9 +30,13 @@ else
    filters = iir_filters(iir_order, output_fs, bands);
 end
 
+filtered = data .* 0;
 for i = 1:length(filters)
     x = fvtool(filters(i).Numerator, filters(i).Denominator);
     x.NormalizedFrequency = 'off';
     x.fs = output_fs;
     x.Name = [mat2str(bands(i,:)) 'Hz'];
+    filtered = filtered + filter(10 ^ (gains(i) / 20) * filters(i).Numerator, filters(i).Denominator, data);
 end
+savedfilename = input('Enter file name <includes path & .wav>:  ', 's');
+audiowrite(savedfilename, filtered, output_fs);
