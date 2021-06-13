d23edf2707526add7d43ce103060c235f25bd04a
MOHAND99 <muhanned122000@yahoo.com>
2021-06-13 16:06:25 +0200

    add subplot

--- plot_time_frequency_domain.m
+++ plot_time_frequency_domain.m
@@ -4,10 +4,15 @@ function plot_time_frequency_domain(data, fs)
     frequency_audio = -fs/2 : df : fs/2-df;
     figure;
     fft_data = fftshift(fft(data)) / length(fft(data));
+    subplot(2, 1, 1);
     plot(frequency_audio, abs(fft_data));
     title('FFT of Input Audio');
     xlabel('Frequency(Hz)');
     ylabel('Amplitude');
 
     % TODO: Plot time domain in the same figure (2 subplots)
+    subplot(2, 1, 2);
+    plot(data)
+    xlabel('Time');
+    ylabel('Amplitude');
 end
