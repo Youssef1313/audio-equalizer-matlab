dc05efd9462177b3f448ffd28209b31276a84b5d
Muhannad Muhammad Bashar <53486163+MOHAND99@users.noreply.github.com>
2021-06-14 12:14:49 +0200

    Resample (#12)
    * add resample lines
    
    * add margin variable
    
    * delete unused var
    
    * change filters fs inputs
    
    * delete unused logic

--- fir_filters.m
+++ fir_filters.m
@@ -2,9 +2,7 @@ function filters = fir_filters(order, fs, bands)
     filters = [];
     for i = 1:length(bands)
        band = bands(i,:) / (fs / 2);
-       if band(2) >= 1
-           return
-       elseif band(1) == 0
+       if band(1) == 0
            b = fir1(order, band(2));
        else
            b = fir1(order, band);
--- main.m
+++ main.m
@@ -12,6 +12,14 @@ plot_time_frequency_domain(data, fs);
 
 
 bands = get_bands();
+
+original_fs = fs;
+if bands(end, end)>= fs/2
+   margin = 11;
+   fs = 2 * (bands(end, end) + margin);
+   data = resample(data, fs, original_fs); 
+end
+
 gains = zeros(1, length(bands));
 for i = 1:length(bands)
     gains(i) = get_number(['Enter gain for ' mat2str(bands(i,:)) 'Hz (between -20 dB and 20 dB): '], @(x) x >= -20 && x <= 20);
@@ -26,24 +34,25 @@ output_fs = get_number('Enter a valid output sample rate: ', @(x) x > 340);
 
 if strcmp('fir',output_fs)
    fir_order = 40;
-   filters = fir_filters(fir_order, output_fs, bands);
+   filters = fir_filters(fir_order, fs, bands);
 else
    iir_order = 4;
-   filters = iir_filters(iir_order, output_fs, bands);
+   filters = iir_filters(iir_order, fs, bands);
 end
 
 acc_filtered = data .* 0;
 for i = 1:length(filters)
     x = fvtool(filters(i).Numerator, filters(i).Denominator);
     x.NormalizedFrequency = 'off';
-    x.fs = output_fs;
+    x.fs = fs;
     x.Name = [mat2str(bands(i,:)) 'Hz'];
     filtered = filter(10 ^ (gains(i) / 20) * filters(i).Numerator, filters(i).Denominator, data);
     plot_time_frequency_domain(filtered, output_fs);
     acc_filtered = filtered + acc_filtered ;
 end
 
-plot_time_frequency_domain(data, fs, acc_filtered, output_fs);
+acc_fitered = resample(acc_filtered, output_fs, fs); %resample to the output fs
+plot_time_frequency_domain(data, original_fs, acc_filtered, output_fs);
 
 [file, path] = uiputfile('*.wav');
 fullFileName = fullfile(path, file);
